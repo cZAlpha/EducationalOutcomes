6f860163a60f1275bf8800c818189644b891579c
Noah <97618802+cZAlpha@users.noreply.github.com>
2024-11-27 16:01:51 -0500

    Got skeleton working

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-   path("notes/dete/<int:pk>", views.NoteDelete.as_view(), name="delete-note"),
+   path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 ]
