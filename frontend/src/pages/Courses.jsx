2c807d59b66d90612e385a1764b520ddcaed1c3e
Noah <97618802+cZAlpha@users.noreply.github.com>
2025-02-25 13:08:32 -0500

    Imported animation library, created AddNewCourseButton component

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import AddCourseButton from "../components/CoursesPage/AddNewCourseButton";
 import { USER } from "../constants";
 
 function Courses() {
@@ -29,7 +30,7 @@ function Courses() {
          <div className="flex flex-col items-center justify-start w-full text-center p-12 min-h-screen bg-gray-100 backdrop-blur-md bg-opacity-[80%]">
             <div /* HeaderContent | Flex row for: Title, AddNewCourseButton*/ className="flex flex-row justify-between items-center w-[60%]">
                <h1 /*Title */ className="text-2xl font-bold">Courses</h1>
-               <button>AddNewCourseButtonPlaceholder</button>
+               <AddCourseButton/>
             </div>
             
             <div>
